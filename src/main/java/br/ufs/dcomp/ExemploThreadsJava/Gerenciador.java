e9c8d32e9754c3adf9ab8a2fceb7a2e3400765a6
Ubuntu <ubuntu@ip-172-31-81-134.ec2.internal>
2025-02-07 23:00:42 +0000

    first commit

--- src/main/java/br/ufs/dcomp/ExemploThreadsJava/Gerenciador.java
+++ src/main/java/br/ufs/dcomp/ExemploThreadsJava/Gerenciador.java
@@ -11,9 +11,11 @@ public class Gerenciador
         Lembrete l1 = new Lembrete("Dorflex", 10, 4);
         Lembrete l2 = new Lembrete("Pracetamol", 8, 3);
         Lembrete l3 = new Lembrete("Piroxican", 4, 8);
+        Lembrete l4 = new Lembrete("Omeprazol", 12, 5);
         
         l1.start();
         l2.start();
         l3.start();
+        l4.start();
     }
 }
